60581e88b2ee8c572813713730e8b9990b4c2955
傅莘莘 <393763323@qq.com>
2016-08-16 17:50:12 +0800

    8.16

--- main.cpp
+++ main.cpp
@@ -5,26 +5,15 @@
 //  Created by 傅莘莘 on 16/8/11.
 //  Copyright © 2016年 傅莘莘. All rights reserved.
 //
-
-#include <fstream>
-#include "symbol_md.hpp"
-using namespace std ;
+#include "md_transform.hpp"
+//提高代码的高复用性
 int main(){
-    string line;
-    ifstream myfile ("/Users/fushenshen/Documents/test.md");
-    cout<<head<<endl;
-    if (myfile.is_open())
-    {
-        while( getline (myfile,line))
-        {
-            if(line==""){
-                continue;
-            }
-            analyze_sentence(line);
-        }
-        
-    }
-    cout<<foot<<endl;
+    using namespace std ;
+    //Users/fushenshen/Documents/test.md
+     std::string line_="/Users/fushenshen/Documents/test.md";
+     std::string out_file="/Users/fushenshen/Documents/out.html";
+    read_tohtml(line_, out_file);
+  
 }
 
 
